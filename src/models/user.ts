2600f69f6d1fa809a834ae7118dd9eaea043c7b0
branmeco <branmeco17@gmail.com>
2023-08-07 18:14:18 -0500

    Signup

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,27 +1,32 @@
-import {Schema, model, models} from 'mongoose';
+import { Schema, model, models } from "mongoose";
 
-new Schema ({
-    email: {
-        type: String,
-        unique: true,
-        required: [true, "Email is required"],
-        match: [
-            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-            "Email is invalid",
-        ]
+const UserSchema = new Schema(
+    {
+        email: {
+            type: String,
+            unique: true,
+            required: [true, "Email is required"],
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                "Email is invalid",
+            ],
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            select: false,
+        },
+        fullname: {
+            type: String,
+            required: [true, "fullname is required"],
+            minLength: [3, "fullname must be at least 3 characters"],
+            maxLength: [20, "fullname must be at most 20 characters"],
+        },
     },
-    password: {
-        type: String,
-        required: [true, "Password is required"],
-        select: false,
-    },
-    fullName: {
-        type: String,
-        required: [true, "Fullname is required"],
-        minLength: [3, "Fullname must be at least 3 characters"],
-        maxLength: [50, "Fullname must be at most 50 characters"],
-    },
-});
+    {
+        timestamps: true,
+    }
+);
 
-const User = models.User || model('User', userSchema);
+const User = models.User || model("User", UserSchema);
 export default User;
